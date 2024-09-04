6b208b0c167f4283592abe646d9b42ef094f50c6
victorguirra <victor.guirra@ubec.edu.br>
2024-09-03 22:05:16 -0300

    update umbriel page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,46 +1,15 @@
-import { useState, useEffect } from 'react';
 import './styles.css';
-
 import umbrielImg from '/umbriel.png';
 
-interface DeferredPromptEvent extends Event {
-    prompt: () => Promise<void>;
-    userChoice: Promise<{
-      outcome: 'accepted' | 'dismissed';
-    }>;
-  }
-
 function Home(){
-    const [deferredPrompt, setDeferredPrompt] = useState<DeferredPromptEvent | null>(null);
-
-    useEffect(() => {
-        const handleBeforeInstallPrompt = (event: Event) => {
-            const e = event as DeferredPromptEvent;
-            e.preventDefault();
-            setDeferredPrompt(e);
-        };
-
-        window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-
-        return () => {
-            window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-        };
-    }, []);
-
-    const handleInstallClick = () => {
-        if (deferredPrompt) {
-            deferredPrompt.prompt();
-            deferredPrompt.userChoice.then(() => {
-                setDeferredPrompt(null);
-            });
-        }
-    };
-
     return(
         <div className="container">
             <h3>Umbriel</h3>
             <img src={umbrielImg} alt="Umbriel" />
-            <button onClick={handleInstallClick}>Instalar App</button>
+            <span className="message">
+                Umbriel é um dos cinco grandes satélites <br></br>
+                de Urano, com um diâmetro de 1170 km.
+            </span>
         </div>
     )
 }
