df961a5093834d1749a558385eebd437dff6e1ac
victorguirra <victor.guirra@ubec.edu.br>
2024-09-03 22:10:35 -0300

    update icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,16 +15,36 @@ export default defineConfig({
         description: 'App para teste de PWA',
         display: 'standalone',
         theme_color: '#FFFFFF',
-        icons: [
+        "icons": [
           {
-            src: 'umbriel.png',
-            sizes: '192x192',
-            type: 'image/png'
+            "src": "umbriel.png",
+            "sizes": "48x48",
+            "type": "image/png"
           },
           {
-            src: 'umbriel.png',
-            sizes: '512x512',
-            type: 'image/png'
+            "src": "umbriel.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "umbriel.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "umbriel.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "umbriel.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ]
       }
